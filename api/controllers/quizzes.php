a4730bc35d7c39eb0caeff52b342a79ee38fe043
mmeacham <maxwmeacham@gmail.com>
2020-04-15 19:38:24 -0400

    database issues

--- api/controllers/quizzes.php
+++ api/controllers/quizzes.php
@@ -2,6 +2,10 @@
 include_once __DIR__ . '/../models/quiz.php';
 header('Content-Type: application/json');
 
+if ($_REQUEST['action'] === 'index') {
+  // echo json_encode(Quizzes::all());
+echo 'test';
+}
 
 
  ?>
